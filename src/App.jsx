d48d6af361bca0e7222edca763367cc571c3b847
Vikram <vikraman@auroville.org.in>
2024-07-24 10:32:01 +0530

    Create Account Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import NavHeader from "./components/NavHeader";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CreateAccount from "./components/CreateAccount";
 
 function App() {
   return (
     <div>
       <NavHeader />
       <Navbar />
+      <div className="max-w-7xl mx-auto">
+        <CreateAccount />
+      </div>
       <Footer />
     </div>
   );
